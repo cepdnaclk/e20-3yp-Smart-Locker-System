5bef90f2f9247d8b2e74ef62390611f37fd9f298
sidantha0377 <150885218+sidantha0377@users.noreply.github.com>
2025-02-28 20:30:55 +0530

    add login page

--- code/apps/webapp/secure_x/src/App.js
+++ code/apps/webapp/secure_x/src/App.js
@@ -1,20 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import logo from './img/logo.png';
-import banner from './img/banner.png';
 import './App.css';
+import Loginsignup from './components/loginsignup/Loginsignup.js'; // Capitalize component names
+import NavigateButton from './components/Button/NavigateButton.js';
 
-
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       
-        <h3>
-        Secure X - Smart Locker System 
-        </h3>
-        
+        <h3>Secure X - Smart Locker System</h3>
+        <NavigateButton to="/login" text="LOG IN >" />
       </header>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Loginsignup />} />
+      </Routes>
+    </Router>
   );
 }
 
