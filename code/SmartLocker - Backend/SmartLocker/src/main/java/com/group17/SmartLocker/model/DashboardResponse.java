4fd55ee483f7eb293af56cbe808e4c2e820d1962
sameera-87 <sameekumarasinghe87@gmail.com>
2025-03-03 09:50:04 +0530

    Dashboard endpoints added
    Basic version of dashboard is added

---
+++ code/SmartLocker - Backend/SmartLocker/src/main/java/com/group17/SmartLocker/model/DashboardResponse.java
@@ -0,0 +1,11 @@
+package com.group17.SmartLocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class DashboardResponse {
+    private String title;
+    private String message;
+}
