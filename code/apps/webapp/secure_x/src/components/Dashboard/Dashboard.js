cb4f6b2cb5c2a0787d01cf62d4d44050a5c7f1a1
sidantha0377 <150885218+sidantha0377@users.noreply.github.com>
2025-03-06 02:52:42 +0530

    add admin controler and usre controlers

--- code/apps/webapp/secure_x/src/components/Dashboard/Dashboard.js
+++ code/apps/webapp/secure_x/src/components/Dashboard/Dashboard.js
@@ -6,10 +6,16 @@ import './Dashboard.css'; // Import a CSS file for layout styling
 import TabBlack from '../Tabs/Black/TabBlack.js';
 import TabWhite from '../Tabs/White/TabWhite.js';
 
+
 const Dashboard = () => {
-  useEffect(() => {
-    alert("Welcome to the Dashboard! \nThis is your main dashboard area.");
-  }, [])
+    useEffect(() => {
+      const hasVisited = localStorage.getItem("hasVisited");
+  
+      if (!hasVisited) {
+        alert("Welcome to the Dashboard! \nThis is your main dashboard area.");
+        localStorage.setItem("hasVisited", "true");
+      }
+    }, []);
   return (
     <div className="dashboard-container">
       
@@ -18,20 +24,23 @@ const Dashboard = () => {
         <div className="main-content">
           <h1>Dashboard</h1>
           <div className='Tablist'>
-            <TabBlack text={"Active users "} number={25} />
-            <TabBlack text={"Total users "} number={150} />
-            <TabBlack text={"Active lockers "} number={40} />
-            <TabBlack text={"Total lockers "} number={50} />
+            <TabBlack text={"Active users "} number={"--"} />
+            <TabBlack text={"Total users "} number={"--"} />
+            <TabBlack text={"Active lockers "} number={1} />
+            <TabBlack text={"Total lockers "} number={1} />
           </div>
           
             <h2>Alerts:</h2>
             <div className='Tablist' >
-              <TabWhite text={"Issus"} number={2}/>
-              <TabWhite text={"Attention need"} number={2}/>
-              <TabWhite text={"Securety alearts"} number={2}/>
+              <TabWhite text={"Issus"} number={"--"}/>
+              <TabWhite text={"Attention need"} number={"--"}/>
+              <TabWhite text={"Securety alearts"} number={"--"}/>
             </div>
             <h2> Recent Activity: </h2>
-            // have to add table real time updated
+            <div className='TablistH' >
+              <TabWhite text={"Check pending users "} number={"--"} />
+            </div>
+            
         </div>
       </div>
     </div>
