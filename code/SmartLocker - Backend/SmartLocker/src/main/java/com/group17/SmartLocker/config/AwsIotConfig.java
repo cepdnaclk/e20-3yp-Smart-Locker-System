0bac63b783443fe091e3bda3b006086fc23bb1e0
sameera-87 <sameekumarasinghe87@gmail.com>
2025-05-29 15:29:56 +0530

    Ready to create a jar file

--- code/SmartLocker - Backend/SmartLocker/src/main/java/com/group17/SmartLocker/config/AwsIotConfig.java
+++ code/SmartLocker - Backend/SmartLocker/src/main/java/com/group17/SmartLocker/config/AwsIotConfig.java
@@ -6,18 +6,28 @@ import com.group17.SmartLocker.Util.KeyStoreUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.InputStream;
+
 
 @Configuration
 public class AwsIotConfig {
 
     private static final String CLIENT_ENDPOINT = "a1u0d4dqvjvus1-ats.iot.ap-southeast-1.amazonaws.com";
     private static final String CLIENT_ID = "3YP-device";
-    private static final String CERT = "SmartLocker/src/main/resources/certs/device-cert.pem.crt";
-    private static final String KEY = "SmartLocker/src/main/resources/certs/private-key.pem";
+//    private static final String CERT = "SmartLocker/src/main/resources/certs/device-cert.pem.crt";
+//    private static final String KEY = "SmartLocker/src/main/resources/certs/private-key.pem";
 
 
     @Bean
     public AWSIotMqttClient awsIotMqttClient() throws Exception{
+
+        // Load cert and key from classpath
+        InputStream CERT = getClass().getClassLoader().getResourceAsStream("certs/device-cert.pem.crt");
+        InputStream KEY = getClass().getClassLoader().getResourceAsStream("certs/private-key.pem");
+
+        if (CERT == null || KEY == null) {
+            throw new RuntimeException("Certificate or key file not found in resources/certs/");
+        }
 
 //        System.out.println("awsIotMqttClient");
         KeyStorePasswordPair pair = KeyStoreUtil.getKeyStorePasswordPair(CERT, KEY);
