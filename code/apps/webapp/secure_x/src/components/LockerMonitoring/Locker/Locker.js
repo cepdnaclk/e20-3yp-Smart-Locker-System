9927de09a7ab04f6c2813c439d047ce5f21c7e81
sidantha0377 <150885218+sidantha0377@users.noreply.github.com>
2025-05-30 08:51:03 +0530

    add delet dialog box and fix dilog box eror

--- code/apps/webapp/secure_x/src/components/LockerMonitoring/Locker/Locker.js
+++ code/apps/webapp/secure_x/src/components/LockerMonitoring/Locker/Locker.js
@@ -1,50 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { getLockerData } from '../../Services/lockerAPI';
-import { SquarePen,Trash2,SquarePlus,KeyRound } from 'lucide-react';
-import Tooltip from '@mui/material/Tooltip';
-import '../../Button/Button.css'
-import '../../TableStyle/Table.css'
-const Locker = () =>  {
+import React, { useState, useEffect } from "react";
+import { getLockerData } from "../../Services/lockerAPI";
+import { SquarePen, Trash2, SquarePlus, KeyRound } from "lucide-react";
+import Tooltip from "@mui/material/Tooltip";
+import "../../Button/Button.css";
+import "../../TableStyle/Table.css";
+const Locker = () => {
+  const [locker, setLocker] = useState([]);
 
-    const [locker, setLocker] = useState([]);
-      
-        // Fetch locker users
-        const handleLocker = async () => {
-        console.log("Fetching locker cluster data");
-        try {
-          const response = await getLockerData();
-          // Adjust the filtering as needed
-          const locker = response.data;
-          setLocker(locker);
-          console.log("Locker Data:", locker);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-          alert(`Invalid Request: Token ${localStorage.getItem("token")}`);
-        }
-      };
-    
-      // Fetch data when the component mounts
-      useEffect(() => {
-        handleLocker();
-      }, []);
+  // Fetch locker users
+  const handleLocker = async () => {
+    console.log("Fetching locker cluster data");
+    try {
+      const response = await getLockerData();
+      // Adjust the filtering as needed
+      const locker = response.data;
+      setLocker(locker);
+      console.log("Locker Data:", locker);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      alert(`Invalid Request: Token ${localStorage.getItem("token")}`);
+    }
+  };
+
+  // Fetch data when the component mounts
+  useEffect(() => {
+    handleLocker();
+  }, []);
 
   return (
-    <div >
+    <div>
       <h2>Lockers</h2>
-      <div className='ActionB'>
-          <Tooltip title = 'Add locker'arrow componentsProps={{tooltip: {sx: {fontSize: '12px',backgroundcolor:'black',color: '#fff'},},}}>
-            <button className='ADDB'><SquarePlus size={20}/></button>
-          </Tooltip>
+      <div className="ActionB">
+        <Tooltip
+          title="Add locker"
+          arrow
+          componentsProps={{
+            tooltip: {
+              sx: { fontSize: "12px", backgroundcolor: "black", color: "#fff" },
+            },
+          }}
+        >
+          <button className="ADDB">
+            <SquarePlus size={20} />
+          </button>
+        </Tooltip>
       </div>
-      <table className='Ctable'>
+      <table className="Ctable">
         <thead>
-          <tr> 
+          <tr>
             <th>LockerID</th>
             <th>Disply Number</th>
             <th>Status</th>
-            <th>Locker Log</th>
+            {/* <th>Locker Log</th> */}
             <th>Locker Cluster</th>
-            
+
             <th>Action</th>
           </tr>
         </thead>
@@ -54,28 +63,69 @@ const Locker = () =>  {
               <td>{user.lockerId}</td>
               <td>{user.displayNumber}</td>
               <td>{user.lockerStatus}</td>
-              <td>{user.lockerLogs}</td>
-              <td>{user.lockerCluster.id}</td>
-              <td className='ActionF'>
-                <Tooltip title = 'Edit'arrow componentsProps={{tooltip: {sx: {fontSize: '12px',backgroundcolor:'black',color: '#fff'},},}}>
-                  <button className='EDITB'><SquarePen size={16} /></button>
+              {/* <td>{user.lockerLogs}</td> */}
+              <td>{user.lockerClusterId}</td>
+              <td className="ActionF">
+                <Tooltip
+                  title="Edit"
+                  arrow
+                  componentsProps={{
+                    tooltip: {
+                      sx: {
+                        fontSize: "12px",
+                        backgroundcolor: "black",
+                        color: "#fff",
+                      },
+                    },
+                  }}
+                >
+                  <button className="EDITB">
+                    <SquarePen size={16} />
+                  </button>
                 </Tooltip>
-                
-                <Tooltip title = 'Unlock'arrow componentsProps={{tooltip: {sx: {fontSize: '12px',backgroundcolor:'black',color: '#fff'},},}}>
-                  <button className='UNLOCKB'><KeyRound size={16} /></button>
+
+                <Tooltip
+                  title="Unlock"
+                  arrow
+                  componentsProps={{
+                    tooltip: {
+                      sx: {
+                        fontSize: "12px",
+                        backgroundcolor: "black",
+                        color: "#fff",
+                      },
+                    },
+                  }}
+                >
+                  <button className="UNLOCKB">
+                    <KeyRound size={16} />
+                  </button>
                 </Tooltip>
-                
-                <Tooltip title = 'Delet'arrow componentsProps={{tooltip: {sx: {fontSize: '12px',backgroundcolor:'black',color: '#fff'},},}}>
-                  <button className='DELETB'><Trash2 size={16}/></button>
+
+                <Tooltip
+                  title="Delet"
+                  arrow
+                  componentsProps={{
+                    tooltip: {
+                      sx: {
+                        fontSize: "12px",
+                        backgroundcolor: "black",
+                        color: "#fff",
+                      },
+                    },
+                  }}
+                >
+                  <button className="DELETB">
+                    <Trash2 size={16} />
+                  </button>
                 </Tooltip>
-                
               </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+};
 
 export default Locker;
