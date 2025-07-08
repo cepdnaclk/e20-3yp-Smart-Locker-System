635690dbe376beaf38f143efcac87f2ade7078fb
sameera-87 <sameekumarasinghe87@gmail.com>
2025-07-08 17:16:20 +0530

    Run file added to again

---
+++ code/SmartLocker - Backend/SmartLocker/src/main/java/com/group17/SmartLocker/SmartLockerApplication.java
@@ -0,0 +1,13 @@
+package com.group17.SmartLocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLockerApplication.class, args);
+	}
+
+}
+ 
