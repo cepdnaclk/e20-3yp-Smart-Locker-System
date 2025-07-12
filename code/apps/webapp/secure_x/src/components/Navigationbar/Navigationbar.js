a972bb9f4cf2dd8e8d97dcb6b6619dd739611644
sidantha0377 <150885218+sidantha0377@users.noreply.github.com>
2025-07-12 08:20:17 +0530

    front end uodate with fogot password

--- code/apps/webapp/secure_x/src/components/Navigationbar/Navigationbar.js
+++ code/apps/webapp/secure_x/src/components/Navigationbar/Navigationbar.js
@@ -1,21 +1,69 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {LayoutDashboard , UserCog ,Binoculars,MapPinned,ChartColumn,ShieldUser,LogOut } from 'lucide-react';
-import "./Navigationbar.css"; 
-// <li><Link to="/lockerconfig">Locker Configuration</Link></li> 
+import {
+  LayoutDashboard,
+  UserCog,
+  Binoculars,
+  MapPinned,
+  CircleUser,
+  ShieldUser,
+  LogOut,
+} from "lucide-react";
+import "./Navigationbar.css";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">Secure X</div>
+
+      {/* Main navigation links */}
       <ul className="nav-links">
-        <li> <Link to="/dashboard"><LayoutDashboard/>  Dashboard</Link></li>
-        <li>  <Link to="/usermanage"><UserCog />  User Management</Link></li>
-        <li> <Link to="/lockermonitoring"><Binoculars />  Locker Monitoring</Link></li>
-        
-        <li><Link to="/map"><MapPinned/>  Map</Link></li>
-        <li><Link to="/randa"><ChartColumn />  Reports & Analytics</Link></li>
-        <li><Link to="/adminmanage"><ShieldUser />  Admin Management</Link></li>
-        <li style={{backgroundColor :'red', color:'white'} }><Link to="/home"><LogOut />  Log Out</Link></li>
+        <li>
+          <Link to="/dashboard" className="normal-link">
+            <LayoutDashboard />
+            Dashboard
+          </Link>
+        </li>
+        <li>
+          <Link to="/usermanage" className="normal-link">
+            <UserCog />
+            User Management
+          </Link>
+        </li>
+        <li>
+          <Link to="/lockermonitoring" className="normal-link">
+            <Binoculars />
+            Locker Monitoring
+          </Link>
+        </li>
+        <li>
+          <Link to="/map" className="normal-link">
+            <MapPinned />
+            Map
+          </Link>
+        </li>
+        <li>
+          <Link to="/adminmanage" className="normal-link">
+            <ShieldUser />
+            Admin Management
+          </Link>
+        </li>
+      </ul>
+
+      {/* User section at bottom */}
+      <ul className="user-section">
+        <li>
+          <Link to="/usercontrol" className="normal-link">
+            <CircleUser />
+            User Profile
+          </Link>
+        </li>
+        <li>
+          <Link to="/home" className="logout-link">
+            <LogOut />
+            Log Out
+          </Link>
+        </li>
       </ul>
     </nav>
   );
